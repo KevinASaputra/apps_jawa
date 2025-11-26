8971cc66d975e9aded9151b77a23a374f5e4cdc3
Kevin <kevinadikasaputragithub@gmail.com>
2025-11-26 23:46:33 +0700

    :wrench: Feat: create refresh token authentication use JWT

---
+++ apps/backend/src/middleware/middleware.auth.ts
@@ -0,0 +1,26 @@
+import type { Context, Next } from "hono";
+import jwt from "jsonwebtoken";
+import { isBlacklisted } from "../utils/tokenStore.js";
+
+export const authMiddleware = async (c: Context, next: Next) => {
+  const authHeader = c.req.header("Authorization");
+
+  if (!authHeader) {
+    return c.json({ message: "Unauthorized: Token is missing" }, 401);
+  }
+
+  const token = authHeader.replace("Bearer", " ");
+
+  if (isBlacklisted(token)) {
+    return c.json({ err: "Token has bee revoked" }, 401)
+  }
+
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET!);
+
+    c.set("user", decode);
+    await next();
+  } catch (err) {
+    return c.json({ err: "Token expired" }, 401)
+  }
+}
